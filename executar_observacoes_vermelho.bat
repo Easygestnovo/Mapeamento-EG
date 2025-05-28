a3d2bb02d3d7140240f7adf9ac33c828a1ef12e3
Easygestnovo <maria.fontainhas@easygest.com.pt>
2025-05-28 19:10:11 +0100

    Add files via upload

---
+++ executar_observacoes_vermelho.bat
@@ -0,0 +1,8 @@
+
+@echo off
+REM Executa o script com Observações a vermelho
+"C:\Users\Maria Fontainhas\AppData\Local\Programs\Python\Python313\python.exe" gerar_html_observacoes_vermelho.py
+
+REM Abre automaticamente o HTML
+start ref_mapping_com_logo.html
+pause
